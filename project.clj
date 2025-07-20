aee3b97292b2ed691752015f73739b4d70f3bae5
Kimbsy <lordkimber@gmail.com>
2025-07-20 15:07:49 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.kimbsy/lein-template.clunk "0.1.7"
+(defproject com.kimbsy/lein-template.clunk "0.1.8"
   :description "A Leiningen template for the clunk game engine"
   :url "https://github.com/Kimbsy/clunk-template"
   :license {:name "MIT License"
--- resources/leiningen/new/clunk/project.clj
+++ resources/leiningen/new/clunk/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "0.6.0"]]
+                 [com.kimbsy/clunk "0.7.0"]]
   :main ^:skip-aot {{name}}.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
