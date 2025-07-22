d325b60ac4d7a8a69841f736cc76fe18423e1c98
Kimbsy <lordkimber@gmail.com>
2025-07-22 20:45:25 +0100

    bump version

--- resources/leiningen/new/clunk/project.clj
+++ resources/leiningen/new/clunk/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "0.8.0"]]
+                 [com.kimbsy/clunk "0.9.0"]]
   :main ^:skip-aot {{name}}.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
